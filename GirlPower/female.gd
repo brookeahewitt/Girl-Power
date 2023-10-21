c5cc69469e3928f1aea1ebdb5c40526059bd330e
Brooke Hewitt <112017131+brookeahewitt@users.noreply.github.com>
2023-10-21 12:55:09 -0400

    Added movement to female sprite

--- GirlPower/female.gd
+++ GirlPower/female.gd
@@ -1,11 +1,40 @@
 extends Node2D
 
+@export var speed = 150 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x < 0:
+		$AnimatedSprite2D.animation = "left"
+	elif velocity.x > 0:
+		$AnimatedSprite2D.animation = "right"
+	elif velocity.y > 0:
+		$AnimatedSprite2D.animation = "forward"
+	elif velocity.y < 0:
+		$AnimatedSprite2D.animation = "back"
